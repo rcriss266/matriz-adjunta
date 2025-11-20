48c0432e50a3cfeefa340ec3af721b22edd9ef91
rcriss266 <rcriss266@gmail.com>
2025-11-19 21:55:23 -0600

    commit numero 2

--- MatrizAdjunta.java
+++ MatrizAdjunta.java
@@ -7,8 +7,8 @@ public class MatrizAdjunta {
         Scanner sc = new Scanner(System.in);
         double[][] m = new double[3][3];
 
-        // Pedir los valores al usuario
-        System.out.println("Ingresa los 9 valores de la matriz 3x3:");
+        
+        System.out.println("Ingresa los 9 valores de la matriz:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print("m[" + i + "][" + j + "]: ");
@@ -16,10 +16,10 @@ public class MatrizAdjunta {
             }
         }
 
-        // Obtener la adjunta
+        
         double[][] adj = adjunta3x3(m);
 
-        // Mostrar matriz adjunta
+        
         System.out.println("\nMatriz Adjunta:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -29,7 +29,7 @@ public class MatrizAdjunta {
         }
     }
 
-    // ---- MATRIZ ADJUNTA 3x3 ---
+   
     public static double[][] adjunta3x3(double[][] m) {
         double[][] adj = new double[3][3];
 
