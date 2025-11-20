429672d1135587622fcec1a54399d1663d511977
rcriss266 <rcriss266@gmail.com>
2025-11-19 21:53:22 -0600

    commit numero 1

---
+++ MatrizAdjunta.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class MatrizAdjunta {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double[][] m = new double[3][3];
+
+        // Pedir los valores al usuario
+        System.out.println("Ingresa los 9 valores de la matriz 3x3:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("m[" + i + "][" + j + "]: ");
+                m[i][j] = sc.nextDouble();
+            }
+        }
+
+        // Obtener la adjunta
+        double[][] adj = adjunta3x3(m);
+
+        // Mostrar matriz adjunta
+        System.out.println("\nMatriz Adjunta:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%8.2f", adj[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    // ---- MATRIZ ADJUNTA 3x3 ---
+    public static double[][] adjunta3x3(double[][] m) {
+        double[][] adj = new double[3][3];
+
+        adj[0][0] =  (m[1][1] * m[2][2] - m[1][2] * m[2][1]);
+        adj[0][1] = -(m[1][0] * m[2][2] - m[1][2] * m[2][0]);
+        adj[0][2] =  (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
+
+        adj[1][0] = -(m[0][1] * m[2][2] - m[0][2] * m[2][1]);
+        adj[1][1] =  (m[0][0] * m[2][2] - m[0][2] * m[2][0]);
+        adj[1][2] = -(m[0][0] * m[2][1] - m[0][1] * m[2][0]);
+
+        adj[2][0] =  (m[0][1] * m[1][2] - m[0][2] * m[1][1]);
+        adj[2][1] = -(m[0][0] * m[1][2] - m[0][2] * m[1][0]);
+        adj[2][2] =  (m[0][0] * m[1][1] - m[0][1] * m[1][0]);
+
+        return adj;
+    }
+}
