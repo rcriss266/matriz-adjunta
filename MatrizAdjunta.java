bad8086ce755c39da92275a21636004410f8a9c9
rcriss266 <rcriss266@gmail.com>
2025-11-20 19:49:13 -0600

    commit que lee la matriz bien

--- MatrizAdjunta.java
+++ MatrizAdjunta.java
@@ -1,35 +1,46 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MatrizAdjunta {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
         double[][] m = new double[3][3];
 
-        
-        System.out.println("Ingresa los 9 valores de la matriz:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("m[" + i + "][" + j + "]: ");
-                m[i][j] = sc.nextDouble();
+        try {
+            Scanner sc = new Scanner(new File("matriz.txt"));
+
+            // Lee los numeros del archivo
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (sc.hasNextDouble()) {
+                        m[i][j] = sc.nextDouble();
+                    } else {
+                        System.out.println("El archivo no tiene suficientes números.");
+                        return;
+                    }
+                }
             }
+            sc.close();
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+            return;
         }
 
-        
+        // Calcula
         double[][] adj = adjunta3x3(m);
 
-        
-        System.out.println("\nMatriz Adjunta:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.printf("%8.2f", adj[i][j]);
-            }
-            System.out.println();
-        }
+        // Imprime la matriz original
+        System.out.println("Matriz original:");
+        imprimirMatriz(m);
+
+        // Imprime la adjunta
+        System.out.println("\nMatriz adjunta:");
+        imprimirMatriz(adj);
     }
 
-   
+    // Función para calcular la  adjunta
     public static double[][] adjunta3x3(double[][] m) {
         double[][] adj = new double[3][3];
 
@@ -47,4 +58,15 @@ public class MatrizAdjunta {
 
         return adj;
     }
+
+    // imprime la   matriz
+    public static void imprimirMatriz(double[][] matriz) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%8.2f", matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
+
